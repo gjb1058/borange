9f446b4c9e13902574a2bd21b5b6ee9fb1789724
root <root@LAPTOP-AE7EBL3L.localdomain>
2019-09-15 00:09:33 -0400

    Created shapes.h and shapes.c with basic lines and boxes functions

---
+++ src/shapes.c
@@ -0,0 +1,40 @@
+
+
+
+void drawHorz(int x, int y, int size, char symbol)
+{
+	move(x,y);
+	for(int col = 0; col < size; col++)
+	{
+		addch(symbol);
+	}
+}
+
+void drawVert(int x, int y, int size, char symbol)
+{
+	move(x,y);
+	for(int row = 0; row <= size; row++)
+	{
+		addch(symbol);
+		move(x+row,y);
+	}
+}
+
+void drawBox(int x, int y, int size, char symbol)
+{
+	move(x,y);
+	drawHorz(x,y,size,symbol);
+	drawVert(x,y,size,symbol);
+	drawHorz(x+x,y,size,symbol);
+	drawVert(x,y+size,size,symbol);
+}
+void drawSquare(int x, int y, int size, char symbol)
+{
+	move(x,y);
+	for(int square = 0; square < size; square++)
+	{
+		drawHorz(x+square,y,size,symbol);
+	}
+}
+
+
