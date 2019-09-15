ef667b8e4c0bb3db6eb4faae2c3a760cc4ba434b
root <root@LAPTOP-AE7EBL3L.localdomain>
2019-09-15 00:15:14 -0400

    Added some stuff to main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,29 @@
 #include "stdio.h"
+#include "shapes.h"
+#include <ncurses.h>
+
+
+
 
 int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
+
+    initscr();
+    cbreak();
+    noecho();
+    printw("Hello world! I'm %s", "a simple test");
+    getch();	//Waits for a char to be entered	
+    drawSquare(10,10,6,'#');
+    drawVert(15,15,5,'t');
+    drawBox(30,30,10,'#');
+    getch();
+
+    clear();
+    endwin();	//Closes the window
+
+ 
     printf("Hello, world!\n");
 
     return 0;
