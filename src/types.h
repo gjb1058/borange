500b8d9d6b58546b99e5cc16b806e457b18ab8f1
gjb1058 <gjb1058@rit.edu>
2019-09-18 15:16:45 -0400

    Created a textures folder, with a default texture pack. Added buildings.

--- src/types.h
+++ src/types.h
@@ -22,22 +22,45 @@ enum Colors {
     WHITE       = 15;
 } COLOR;
 
+enum LabTypes {
+    MILITARY = 0;
+    INDUSTRY = 0;
+} TYPE_L;
+
 enum ResourceTypes {
     WORLD   = 0;
     CRYSTAL = 1;
     FOOD    = 2;
     ENERGY  = 3;
     EXOTIC  = 4;
-} TYPE;
+} TYPE_R;
 
-struct Resource {
+enum BuildingTypes {
+    HOUSE       = 1;
+    FACTORY     = 2;
+    CHURCH      = 3;
+    MARKET      = 4;
+    BARRACKS    = 5;
+    LAB         = 6;
+    MINE        = 7;   
+} TYPE_B;
+
+struct Building {
+    TILE*[][] tiles;
+    bool canProduce;
+    bool canResearch;
+    TYPE_B type;    
+} BUILDING;
+
+struct Tile {
     char graphic;
-    TYPE type;
     bool canObtain;
     bool canDestroy;
     bool canTraverse;
+    bool buildingBlock;
     COLOR foreground;
     COLOR background;
-    
-}
+    TYPE_R type;
+    BUILDING building;
+} TILE;
 #endif
